16c0cd1e20daf6e45391118ad1cb86a65a30bc3e
knoxgon <volkan.guven@hotmail.se>
2019-05-03 14:32:33 +0200

    Implemented corresponding definitions for the other class functions

--- Experience.cpp
+++ Experience.cpp
@@ -28,3 +28,82 @@ Experience & Experience::operator=(Experience other)
 	}
 	return *this;
 }
+
+void Experience::setLevel(int level)
+{
+	this->level = level;
+}
+
+void Experience::setExperience(int exp)
+{
+	this->experience = exp;
+}
+
+void Experience::setMaxExperience(int maxexp)
+{
+	this->maxExperience = maxexp;
+}
+
+void Experience::updateExperience(int exp)
+{
+	if (exp + this->getExperience() > this->getMaxExperience()) {
+		int restExp = this->getMaxExperience() - this->getExperience();
+		this->level++;
+		this->setExperience(0);
+		this->setMaxExperience(this->getNextExperience(this->getLevel()));
+		exp -= restExp;
+		this->experience += exp;
+	}
+	else if (exp < 0){}
+	else {
+		this->experience += exp;
+		this->experienceControlFlow();
+	}
+}
+
+void Experience::experienceControlFlow() {
+	if (this->getExperience() >= this->getMaxExperience()) {
+		this->level++;
+		this->setExperience(0);
+		this->setMaxExperience(this->getNextExperience(this->getLevel()));
+	}
+}
+
+void Experience::updateLevel()
+{
+	this->level++;
+}
+
+int Experience::getLevel() const
+{
+	return this->level;
+}
+
+int Experience::getExperience() const
+{
+	return this->experience;
+}
+
+int Experience::getMaxExperience() const
+{
+	return this->maxExperience;
+}
+
+int Experience::getNextExperience(int level)
+{
+	if (level > 90)
+		return (int)std::floor(11.712*(std::pow(level + 2, 3)*std::log(level + 2) + 100) + 0.5);
+	else
+		return (int)std::floor((std::pow(level + 2, 3)*std::log(level + 2) + 100) + 0.5);
+}
+
+std::string Experience::convertExperienceToString(int currentLevel)
+{
+	return std::to_string(this->getNextExperience(currentLevel));
+}
+
+std::string Experience::toString() const
+{
+	return "Level: " + std::to_string(this->getLevel()) + 
+		"\nExp: " + std::to_string(this->getExperience()) + '/' + std::to_string(this->getMaxExperience()) + '\n';
+}
