233cba77a61f80624fb87fc52913ddf98a4b4cf9
knoxgon <volkan.guven@hotmail.se>
2019-05-04 17:46:17 +0200

    Constructor class declarations and defined some of the definitions inside the class

--- Gear.h
+++ Gear.h
@@ -34,4 +34,14 @@ private:
 
 	BonusStats* bonStats;
 
+public:
+	Gear();
+	Gear(NS_GearAttributes::Rarity g_rarity, BonusStats* bonStats = nullptr, int itemLvl = 0,
+		int currdur = 0, int maxdur = 0, int reqstr = 0, int reqint = 0, int reqend = 0);
+	~Gear();
+
+	bool operator==(const Gear& gear) {
+		return this->g_rarity == gear.g_rarity
+			&& this->itemLevel == gear.itemLevel;
+	}
 };
