81f3faab08d9cb6cc918e74dd0558a2132459e1e
knoxgon <volkan.guven@hotmail.se>
2019-05-04 17:40:56 +0200

    Added the sufficient function declarations

--- UserStats.h
+++ UserStats.h
@@ -15,4 +15,27 @@ public:
 	virtual ~UserStats();
 	UserStats(int attackPower, int defendPower, int healthPoint, int maxHP, int manaPoint, int maxMP,
 		int strength, int intelligence, int endurance);
+
+	int getStrength()const;
+	int getIntelligence()const;
+	int getEndurance()const;
+
+	void setStrength(int str);
+	void setIntelligence(int intel);
+	void setEndurance(int endur);
+
+	void setManaPoint(int manaPoint);
+	void setMaxManaPoint(int manaPoint);
+
+	int getManaPoint()const;
+	int getMaxManaPoint()const;
+
+	void updateManaPoint(int manavalue);
+	void updateMaxManaPoint(int maxmanavalue);
+
+	void updateStrength(int strvalue);
+	void updateIntelligence(int intvalue);
+	void updateEndurance(int endvalue);
+
+	std::string toString() const;
 };
